bb5836d2a1d242de3f3251a20508d54dd78d1b20
shiina <ben410332@gmail.com>
2021-03-15 19:11:57 +0800

    test1_0315

---
+++ src/app.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import http from 'http';
+import cookieParser from 'cookie-parser';
+import path from 'Path';
+import cors from 'cors';
+import morgan from 'morgan';
+import passport from 'passport';
+import session from 'express-session';
+
+const app = express();
+
+require('doenv').config();
+
+app.use(express.json)
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(express.static(path.resolve(__dirname, '../view')));//路由
+app.use(morgan('dev'));//看API正不正確
+app.use(cors());//看哪裡要接
+
+
+app.use(session({
+    secret:process.env.APP_KEY,//密鑰
+    resave:false,
+    saveUninitialized:false,
+}))
+// 必須要有22~26行，才能執行
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/api',(req, res ,next)=>{
+    res.status(200).json({message:'Kusa'});
+});
+
+//port rouge (1~65536)
+//mssql port 1433
+
+const server = http.createServer(app);
+
+server.listen(3000);
+server.on('listening',()=>{
+    const address = server.address();
+    console.log(`This server is on ${address.port}`)
+});
