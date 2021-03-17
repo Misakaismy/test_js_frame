c13456de96d8a4eaf84dc0dd9d243978690350d6
shiina <ben410332@gmail.com>
2021-03-17 20:16:36 +0800

    fix post

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,13 +1,36 @@
 import {Router} from 'express';
 // import {controller} from '../controllers/user.js'
 
+class UserController{
+    getuser = (req, res)=>{
+        const name = "Jack";
+        const {token} = req.query;
+        res.status(200).json({message:`${token}.${name}`});
+    }
+
+    postuser = (req, res)=>{
+        const {body} = req;
+        const {name, phone} = body;
+        const profile = {
+            name,
+            phone,
+        }
+        res.status(200).json({profile});
+        console.log(body);
+        console.log(req);
+    } 
+}
+
+const controller = new UserController;
+
 const router = Router();
 
-router.use('/', (req,res)=>{
-    res.status(200).json({message:'Hi'});
-});
+// router.use('/', (req,res)=>{
+//     res.status(200).json({message:'Hi'});
+// });
 
-// router.get('/', controller.getuser);
-// router.post('/', controller.postuser);
+router.get('/GET', controller.getuser);
+router.post('/POST', controller.postuser);
 
 export default router;
+
